7eaac597a30d8a86a13920a465a9d0751ae9039a
liciolentimo <lentimo@gmail.com>
2019-08-07 12:29:54 +0300

    added fragments for destinations

---
+++ app/src/main/java/com/lentimosystems/licio/blesbokandroid/fragments/MaraFragment.java
@@ -0,0 +1,30 @@
+package com.lentimosystems.licio.blesbokandroid.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lentimosystems.licio.blesbokandroid.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MaraFragment extends Fragment {
+
+
+    public MaraFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mara, container, false);
+    }
+
+}
